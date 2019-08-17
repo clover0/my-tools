c9ac68464ba5967af0e9c749bd532fc6baed1f2c
koenuma <-->
2019-08-17 15:34:59 +0900

    init

---
+++ shell/config.fish
@@ -0,0 +1,9 @@
+set PATH /usr/local/bin /usr/sbin  ~/.nodebrew/current/bin ~/tools/bin ~/Library/Python/3.7/bin/ $PATH
+set PATH $PATH /Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/bin
+set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home
+# Go
+set -x  GOPATH $HOME/go
+set -x GOENV_ROOT $HOME/.goenv
+set PATH $GOENV_ROOT/shims $PATH
+set PATH $GOPATH/bin $PATH
+eval (goenv init - | source)
