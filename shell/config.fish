112e0dd482f27d950d56d19fb1bd5dc6b94babce
koenuma <-->
2019-08-20 23:47:02 +0900

    update

--- shell/config.fish
+++ shell/config.fish
@@ -1,6 +1,10 @@
 set PATH /usr/local/bin /usr/sbin  ~/.nodebrew/current/bin ~/tools/bin ~/Library/Python/3.7/bin/ $PATH
 set PATH $PATH /Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/bin
 set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home
+#rbenv
+#set -x PATH $HOME/.rbenv/bin $PATH
+#status --is-interactive; and source (rbenv init -|psub)
+
 # Go
 set -x  GOPATH $HOME/go
 set -x GOENV_ROOT $HOME/.goenv
