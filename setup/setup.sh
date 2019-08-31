5a4cc93b9614d74579abbdc1132f3713dc5a0c93
koenuma <-->
2019-08-31 12:15:50 +0900

    shell for setup mac

---
+++ setup/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# XCode install
+
+brew install fzf
+
+brew install fish
+chsh -s /usr/local/bin/fish
+echo $SHELL
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+
+fisher add edc/bass
+
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts/
+
+fisher add oh-my-fish/theme-agnoster
+fisher add jethrokuan/z
