e6050cc529d1dbbb946449e04022a10dd89fbba1
Koenuma <clover0@users.noreply.github.com>
2020-06-27 00:28:42 +0900

    fix add user

--- setup/setup.sh
+++ setup/setup.sh
@@ -43,10 +43,12 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo chmod +x /usr/local/bin/docker-compose
+sudo adduser $(whoami) docker
 
 # docker compose 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
 
 # dir name
 LANG=C xdg-user-dirs-gtk-update
