db7ca5a880a2d6e95b5c4e8bc56145e29d712340
Koenuma <clover0@users.noreply.github.com>
2020-06-27 00:22:24 +0900

    add linux steps

--- setup/setup.sh
+++ setup/setup.sh
@@ -20,9 +20,33 @@ rm -rf fonts/
 fisher add oh-my-fish/theme-agnoster
 fisher add jethrokuan/z
 
-# Linux
-#Ubuntu
+####### Linux ########
+### Ubuntu
+sudo apt-get update
 sudp apt install git curl fish 
 
 # default shell
 chsh -s /usr/bin/fish
+
+# docker
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo chmod +x /usr/local/bin/docker-compose
+
+# docker compose 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# dir name
+LANG=C xdg-user-dirs-gtk-update
